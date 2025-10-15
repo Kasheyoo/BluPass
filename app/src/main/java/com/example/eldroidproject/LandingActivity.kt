faa6bcd16663124906a2999b78caffc8dd4b3d34
Kharl <kharlbinagatan86@gmail.com>
2025-10-16 06:14:28 +0800

    Initial commit

---
+++ app/src/main/java/com/example/eldroidproject/LandingActivity.kt
@@ -0,0 +1,11 @@
+package com.example.eldroidproject
+
+import android.app.Activity
+import android.os.Bundle
+
+class LandingActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_landing)
+    }
+}
