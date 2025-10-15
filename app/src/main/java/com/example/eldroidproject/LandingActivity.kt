5df0e930d2cc97115475026fc091b2b2e7866ec9
Kharl <kharlbinagatan86@gmail.com>
2025-10-16 07:22:02 +0800

    firse

--- app/src/main/java/com/example/eldroidproject/LandingActivity.kt
+++ app/src/main/java/com/example/eldroidproject/LandingActivity.kt
@@ -1,11 +1,26 @@
 package com.example.eldroidproject
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class LandingActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landing)
+
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+
+        btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/eldroidproject/LoginActivity.kt
+++ app/src/main/java/com/example/eldroidproject/LoginActivity.kt
@@ -1,12 +1,21 @@
 package com.example.eldroidproject
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 
 class LoginActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        val btnSignUp = findViewById<Button>(R.id.btnSignUp)
+
+
+        btnSignUp.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
